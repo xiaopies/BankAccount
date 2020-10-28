e3faacdf9a465109ddd452c5930db59cbdbb0dab
xiaopies <xiaoshenma2016@gmail.com>
2020-10-28 04:01:20 -0400

    added toString method

--- BankAccount.java
+++ BankAccount.java
@@ -27,11 +27,14 @@ public class BankAccount{
     return false;
   }
   public boolean withdraw(double amount){
-    if (amount>=0 && amount <=b){
+    if (amount>=0 && amount <=balance){
       balance-=amount;
       return true;
     }
     return false;
+  }
+  public String toString(){
+    return accountID + "\t" + balance;
   }
 
 
