5ffb23a6e25cd89ea4c2ae0e4e2ee4732e402d5f
xiaopies <xiaoshenma2016@gmail.com>
2020-10-28 03:52:13 -0400

    added constructor and initializd values

--- BankAccount.java
+++ BankAccount.java
@@ -4,7 +4,12 @@ public class BankAccount{
   private int accountID;
   private String password;
 
-  
+  public BankAccount(double b, int a, String p){
+    balance = b;
+    b = 0;
+    accountID = a;
+    password = p;
+  }
 
 
 }
